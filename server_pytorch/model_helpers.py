96d691649ad25e9bad3ee1c72c2b8b414ffdbfd1
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-06-13 20:53:03 +1000

    converted from tensorflow to pytorch as Render (hosting service) did not provide full support for tensorflow

---
+++ server_pytorch/model_helpers.py
@@ -0,0 +1,68 @@
+import numpy as np
+from unsplash_api import fetch_imgs, load_img_from_url
+import torch
+import torchvision
+
+weights = torchvision.models.ResNet50_Weights.DEFAULT
+preprocess = weights.transforms()
+
+def load_models(classifier_path, siamese_path):
+    """
+    Loads that classification model and siamese model.
+    """
+    classification_model = torch.load(classifier_path)
+    siamese_model = torch.load(siamese_path)
+    return classification_model, siamese_model
+
+
+def process_img(img):
+    """
+    Processes the image into an appropriate format for inference (resnet50).
+    """
+    x = preprocess(img).unsqueeze(0)
+    return x
+
+
+def get_similar(classification_model, siamese_model, input_img, n):
+    """
+    Finds n similar images of an input_img
+    """
+    
+    # process image for inference
+    x = process_img(input_img)
+    
+    # get classification label
+    prediction = classification_model(x).squeeze(0).softmax(0)
+    class_id = prediction.argmax().item()
+    score = prediction[class_id].item()
+    category_name = weights.meta["categories"][class_id]
+    print(f"{category_name}: {100 * score:.1f}%")
+
+    # retrieve comparison images
+    img_urls = fetch_imgs(category_name, n_pages=5)
+    print(f"{len(img_urls)} images retrieved.")
+
+    # compare images using siamese model
+    sims = []
+
+    for index, img_url in enumerate(img_urls):
+        img = load_img_from_url(img_url)
+        img = process_img(img)
+        sim_score = siamese_model(x, img)
+        sims.append({
+            "comp": img,
+            "url": img_url,
+            "sim_score": sim_score,
+        })
+        print(f"Image {index} done.")
+
+    # Sort images by similarity score
+    sims.sort(key=lambda x: x["sim_score"])
+    
+    # Get top n similar images
+    sim_images = sims[:n]
+    sim_images = [x["url"] for x in sim_images]
+    print("Done!")
+    return {
+        "result": sim_images
+    }
---
+++ server_pytorch/unsplash_api.py
@@ -0,0 +1,21 @@
+import numpy as np
+import requests
+import urllib.request as urlreq
+from PIL import Image
+from io import BytesIO
+
+client_id = "1Rb3JZ4ZLqv1ps70yBqjxmpvWkoOeORD2mnh5UekJGk"
+
+def fetch_imgs(keyword, n_pages=3):
+  img_urls = []
+  for i in range(1, n_pages+1):
+    url = f"https://api.unsplash.com/search/photos?page={i}&query={keyword}&client_id={client_id}"
+    results = requests.get(url).json()["results"]
+    for x in results:
+      img_urls.append(x["urls"]["small"])
+  return img_urls
+
+def load_img_from_url(url):
+  response = requests.get(url)
+  image = Image.open(BytesIO(response.content))
+  return image
