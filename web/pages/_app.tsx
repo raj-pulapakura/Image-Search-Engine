8a6bfdf4f56eb66f462e3a03d0fd7002e5eb7c96
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-06-19 16:43:35 +1000

    configured web app with tailwindcss and purgecss

---
+++ web/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/index.css'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
